22566545db5f44e270ecb97167b97fd43f9a0db4
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-08-28 23:38:15 -0300

    section 363 finished

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,9 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AuthStatus } from '../interfaces/auth-status.enum';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
 
-  console.log('route',route)
-  console.log('state',state)
+  const authService = inject( AuthService )
+  const router = inject( Router )
 
-  return true;
+
+  if ( authService.authStatus() === AuthStatus.authenticated ) {
+    return true
+  }
+
+  router.navigateByUrl('/auth/login')
+  return false;
 };
